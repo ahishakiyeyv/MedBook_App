5bca000dbee216799ba840b52edb5167349f5d6f
ahishakiyeyv <yvanahishakiye6@gmail.com>
2023-03-23 11:47:46 +0200

    adding patient controller

---
+++ app/Http/Controllers/PatientController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Patient;
+use Illuminate\Http\Request;
+
+class PatientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //creating a patient
+        $patient = new Patient([
+            'nom_patient'=>$request->get('nom_patient'),
+            'prenom_patient'=>$request->get('nom_etu'),
+            'email'=>$request->get('email'),
+            'telephone'=>$request->get('telephone'),
+            'adresse'=>$request->get('adresse'),
+            'password'=>$request->get('password'),
+            'status'=>0
+        ]);
+        $patient->save();
+        $reponse= [
+            'success'=>true,
+            'data'=>$patient,
+            'message'=>"Patient enregistre avec succes"
+        ];
+        return response()->json($reponse,200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        //showing the patient's data
+        $patient = Patient::all();
+        return  $patient;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //updating the patient's data
+        $patient = new Patient([
+            'nom_patient'=>$request->get('nom_patient'),
+            'prenom_patient'=>$request->get('nom_etu'),
+            'email'=>$request->get('email'),
+            'telephone'=>$request->get('telephone'),
+            'adresse'=>$request->get('adresse'),
+            'password'=>$request->get('password'),
+            'status'=>0
+        ]);
+        $patient = Patient::findOrFail($id);
+        $patient->update();
+        $reponse= [
+            'success'=>true,
+            'data'=>$patient,
+            'message'=>"Patient modifie avec succes"
+        ];
+        return response()->json($reponse,200);
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //Deleting the patient's data
+        $delete = Patient::findOrFail($id);
+         $delete->delete();
+    }
+}
