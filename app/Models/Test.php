509fbba98dff57f657e7eaee41a92a855510b8c0
ahishakiyeyv <yvanahishakiye6@gmail.com>
2023-03-23 14:28:29 +0200

    model added

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Test extends Model
 {
     use HasFactory;
+    protected $table = 'test';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'nom_test',
+        'prix_test',
+        'description'
+    ];
 }
