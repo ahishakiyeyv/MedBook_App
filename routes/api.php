3f40e7934683ad0b08a8b8ee991c0c55bff1d827
ahishakiyeyv <yvanahishakiye6@gmail.com>
2023-03-24 09:05:15 +0200

    api created

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\MedecinController;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\InfirmierController;
+use App\Http\Controllers\RendezvousController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +22,40 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::controller(MedecinController::class)->group(function(){
+    Route::post('/create_medecin','create');
+    Route::get('/medecin','show');
+    Route::put('/update_medecin/{id}','update');
+    Route::post('/delete_medecin/{id}','destroy');
+});
+Route::controller(PatientController::class)->group(function(){
+    Route::post('/create_patient','create');
+    Route::get('/patient','show');
+    Route::put('/update_patient/{id}','update');
+    Route::post('/delete_patient/{id}','destroy');
+});
+Route::controller(InfirmierController::class)->group(function(){
+    Route::post('/create_infirmier','create');
+    Route::get('/infirmier','show');
+    Route::put('/update_infirmier/{id}','update');
+    Route::post('/delete_infirmier/{id}','destroy');
+});
+Route::controller(ServiceController::class)->group(function(){
+    Route::post('/create_service','create');
+    Route::get('/service','show');
+    Route::put('/update_service/{id}','update');
+    Route::post('/delete_service/{id}','destroy');
+});
+Route::controller(TestController::class)->group(function(){
+    Route::post('/create_test','create');
+    Route::get('/test','show');
+    Route::put('/update_test/{id}','update');
+    Route::post('/delete_test/{id}','destroy');
+});
+Route::controller(RendezvousController::class)->group(function(){
+    Route::post('/create_appointment','create');
+    Route::get('/appointment','show');
+    Route::put('/update_appointment/{id}','update');
+    Route::post('/delete_appointment/{id}','destroy');
 });
